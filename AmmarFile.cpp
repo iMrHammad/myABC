54c9eb66c0d3d1e75d7d113c4bab0d24b04af85b
HammadMaqbool <hammad_mqbl@outlook.com>
2021-07-16 23:32:16 -0700

    New file of student

---
+++ AmmarFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	int ProgramStatus=1;
+	while(ProgramStatus==1)
+	{
+		system("CLS");
+		int choice, limit;
+		cout<<"Enter the Number to print the table";
+		cin>>choice;
+		cout<<"Enter table limit";
+		cin>>limit;
+		for(int i =1; i<=limit; i++)
+		{
+			cout<<choice<<" x "<<i<<" ="<<i*choice<<endl;
+		}
+	cout<<"Do you want to print any other table\nPress 1 to Continue any other key to exit...";
+	cin>>ProgramStatus;
+}
+}
